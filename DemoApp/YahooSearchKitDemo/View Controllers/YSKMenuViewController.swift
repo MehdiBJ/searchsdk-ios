cb8e7138047e3f8082e74c6371a79a835fc2de25
Anand Vasudevan <anandvas@yahoo-inc.com>
2015-03-06 18:51:01 -0800

    Developer preview

---
+++ DemoApp/YahooSearchKitDemo/View Controllers/YSKMenuViewController.swift
@@ -0,0 +1,14 @@
+//
+//  YSKMenuViewController.swift
+//  YahooSearchKitDemo
+//
+//  Copyright 2015 Yahoo! Inc.
+//  Licensed under the terms of the zLib license. See LICENSE file at the root of this project for license terms.
+//
+
+import UIKit
+
+class YSKMenuViewController: UIViewController {
+
+
+}
